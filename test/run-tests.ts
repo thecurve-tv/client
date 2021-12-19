7207db24fcb38811320bbbaae803c6cdfa6e1859
Victor <olaitanvictor99@gmail.com>
2021-12-18 20:03:00 -0500

    Provide nodejs tsconfig for tests

--- test/run-tests.ts
+++ test/run-tests.ts
@@ -2,7 +2,7 @@ import { exit } from 'process'
 import { exec, run } from './run-script'
 
 async function main() {
-  const [serverProcess] = exec('cd .server && npm run start:test')
+  const [serverProcess] = exec('cd ../.server && npm run start:test')
   let returnCode = 1
   try {
     returnCode = (await run('npm run ci:e2e')).returnCode
